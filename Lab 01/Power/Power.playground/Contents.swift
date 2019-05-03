4626ae0505a218faffcb393f4ca91703c2412b92
Abd elrhman Arafa Hassanien <abdoarafa72@yahoo.com>
2019-05-03 13:09:46 +0200

    get Power

--- Lab 01/Power/Power.playground/Contents.swift
+++ Lab 01/Power/Power.playground/Contents.swift
@@ -10,15 +10,15 @@ print("Result Using loop",pow(2,7))
 //////////////////////////////////////////////////
 func powRecursion (base:Int, power:UInt) -> Int
 {
-    var m :Int = 1;
+    var result :Int = 1;
     if (power == 0)
     {
         return 1;
     }
     if (power % 2 == 0)
     {
-        m = powRecursion(base: base, power: power / 2);
-        return m * m;
+        result = powRecursion(base: base, power: power / 2);
+        return result * result;
     } else
     {
     return base * powRecursion(base: base, power: power - 1);
